e28c17dcfedc75cd4116ae3fb0359b5b82d15b68
18137036 <9wjdgh9@naver.com>
2023-01-18 21:47:50 +0900

    Feat : github 배포

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'), // './dist'의 절대 경로를 리턴합니다.
+    path: path.resolve(__dirname, 'docs'), // './dist'의 절대 경로를 리턴합니다.
     filename: 'app.bundle.js',
   },
   module: {
